2bc72d4aaf9c50d48e4023044a6893556c45f801
tau <tau.dangngoc@teragroup.io>
2022-12-17 16:23:19 +0700

    setup infra and api creating test settings

---
+++ TestPlatform/src/BuildingBlocks/VietGeeks.TestPlatform.AspNetCore/Services/ITenant.cs
@@ -0,0 +1,8 @@
+﻿namespace VietGeeks.TestPlatform.AspNetCore;
+
+public interface ITenant
+{
+    string UserId { get; }
+
+    string Email { get; }
+}
---
+++ TestPlatform/src/Services/TestManager/VietGeeks.TestPlatform.TestManager.Contract/TestViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace VietGeeks.TestPlatform.TestManager.Contract
+{
+    public class NewTestViewModel
+    {
+        public string TestName { get; set; }
+
+        public string CategoryId { get; set; }
+
+        public string LanguageId { get; set; }
+
+        public string Description { get; set; }
+    }
+
+    public class UpdateTestViewModel
+    {
+
+    }
+
+    public class TestViewModel
+    {
+        public string Id { get; set; }
+
+        public string TestName { get; set; }
+
+        public string CategoryId { get; set; }
+
+        public string LanguageId { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime ModifiedOn { get; set; }
+    }
+}
