c58c7fcda85269cdbfb3e15d899bae1ca2e1f579
tau <tau.dangngoc@teragroup.io>
2022-12-20 09:27:00 +0700

    Added configuration api

--- TestPlatform/src/Services/TestManager/VietGeeks.TestPlatform.TestManager.WebApp/ClientApp/apps/test-manager/src/environments/environment.ts
+++ TestPlatform/src/Services/TestManager/VietGeeks.TestPlatform.TestManager.WebApp/ClientApp/apps/test-manager/src/environments/environment.ts
@@ -4,6 +4,9 @@ export const environment = {
     testManagerApiBaseUrl: "http://localhost:8087",
     auth: {
         domain: 'dev-kz3mgkb4xl50tobe.us.auth0.com',
-        clientId: 'SaMKFPQSdPoAyooI3zF6oiz6zdZ6hU18'
+        clientId: 'SaMKFPQSdPoAyooI3zF6oiz6zdZ6hU18',
+        audience:  'http://test-manager',
+        scope: 'read:current_user',
+        intercepters: ['http://localhost:8087/*']
     }
 }
--- TestPlatform/src/Services/TestManager/VietGeeks.TestPlatform.TestManager.WebApp/ClientApp/libs/shared/src/lib/shared.module.ts
+++ TestPlatform/src/Services/TestManager/VietGeeks.TestPlatform.TestManager.WebApp/ClientApp/libs/shared/src/lib/shared.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [CommonModule, HttpClientModule],
   exports: [
+    HttpClientModule
   ]
 })
 export class SharedModule {}
