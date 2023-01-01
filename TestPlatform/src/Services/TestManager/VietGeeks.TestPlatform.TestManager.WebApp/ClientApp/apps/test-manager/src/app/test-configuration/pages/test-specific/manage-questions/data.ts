55feae9fd3da47dc6a5bf19170f15f99521d73ff
lang song vu <songvu.lang@live.com>
2023-01-01 07:27:27 +0700

    Manage questions first draft

---
+++ TestPlatform/src/Services/TestManager/VietGeeks.TestPlatform.TestManager.WebApp/ClientApp/apps/test-manager/src/app/test-configuration/pages/test-specific/manage-questions/data.ts
@@ -0,0 +1,37 @@
+const questions = [
+  {
+    id: 1,
+    questionNo: 1,
+    questionDefinition: "What is the current officially recognized elevation of Mount Everest?",
+    categoryId: "Geography",
+    categoryName: "Geography",
+    categoryColor: "Geography",
+    answerType: 1,
+    answerTypeName: "Single choice",
+    answers: [
+    {
+      answerDescription: "8488 m",
+      answerPoint: 2,
+    },
+    {
+      answerDescription: "8466 m",
+      answerPoint: 2,
+    },
+    {
+      answerDescription: "8844 m",
+      answerPoint: 2,
+    }],
+    scoreSettings: {
+      correctPoint: 1,
+      incorrectPoint: 1,
+      isPartialAnswersEnabled: false,
+      maxPoints: 1,
+      maxWords: 2
+    },
+    isMandatory: true,
+    createdDate: '2022-12-20T00:00:00',
+    lastModifiedDate: '2022-12-20T00:00:00'
+  }
+];
+
+export { questions };
--- TestPlatform/src/Services/TestManager/VietGeeks.TestPlatform.TestManager.WebApp/ClientApp/apps/test-manager/src/app/test-configuration/state/test.model.ts
+++ TestPlatform/src/Services/TestManager/VietGeeks.TestPlatform.TestManager.WebApp/ClientApp/apps/test-manager/src/app/test-configuration/state/test.model.ts
@@ -10,6 +10,35 @@ export interface TestBasicSetting {
   description: string;
 }
 
+export interface TestQuestion {
+  id: number;
+  questionNo: number;
+  questionDefinition: string;
+  categoryId: string;
+  categoryName: string;
+  categoryColor: string;
+  answerType: number;
+  answerTypeName: string;
+  answers?: Answer[];
+  scoreSettings: ScoreSettings;
+  isMandatory: boolean;
+  createdDate: string;
+  lastModifiedDate: string;
+}
+
+interface ScoreSettings {
+  correctPoint: number;
+  incorrectPoint: number;
+  isPartialAnswersEnabled: boolean;
+  maxPoints: number;
+  maxWords: number;
+}
+
+interface Answer {
+  answerDescription: string;
+  answerPoint: number;
+}
+
 export function createTest(params: Partial<Test>) {
   return {
     id: params.id,
