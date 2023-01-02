fb134405b0f413cb5ad02b778bf277d972ed7776
songvu.lang@gmail.com <songvu.lang@gmail.com>
2023-01-02 14:52:01 +0000

    Show answers

--- TestPlatform/src/Services/TestManager/VietGeeks.TestPlatform.TestManager.WebApp/ClientApp/apps/test-manager/src/app/test-configuration/pages/test-specific/manage-questions/data.ts
+++ TestPlatform/src/Services/TestManager/VietGeeks.TestPlatform.TestManager.WebApp/ClientApp/apps/test-manager/src/app/test-configuration/pages/test-specific/manage-questions/data.ts
@@ -31,6 +31,39 @@ const questions = [
     isMandatory: true,
     createdDate: '2022-12-20T00:00:00',
     lastModifiedDate: '2022-12-20T00:00:00'
+  },
+  {
+    id: 2,
+    questionNo: 2,
+    questionDefinition: "What is the current officially recognized elevation of Mount Everest?",
+    categoryId: "Geography",
+    categoryName: "Geography",
+    categoryColor: "Geography",
+    answerType: 2,
+    answerTypeName: "Multiple choices",
+    answers: [
+    {
+      answerDescription: "8488 m",
+      answerPoint: 2,
+    },
+    {
+      answerDescription: "8466 m",
+      answerPoint: 2,
+    },
+    {
+      answerDescription: "8844 m",
+      answerPoint: 2,
+    }],
+    scoreSettings: {
+      correctPoint: 1,
+      incorrectPoint: 1,
+      isPartialAnswersEnabled: false,
+      maxPoints: 1,
+      maxWords: 2
+    },
+    isMandatory: true,
+    createdDate: '2022-12-20T00:00:00',
+    lastModifiedDate: '2022-12-20T00:00:00'
   }
 ];
 
