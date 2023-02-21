3151edc27e7f6573914c74585fef3d95636e51d0
tau <tau.dangngoc@teragroup.io>
2023-02-21 12:00:33 +0700

    fix build mode for shared module

--- TestPlatform/src/Services/TestManager/VietGeeks.TestPlatform.TestManager.WebApp/ClientApp/libs/shared/src/index.ts
+++ TestPlatform/src/Services/TestManager/VietGeeks.TestPlatform.TestManager.WebApp/ClientApp/libs/shared/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/shared.module';
 export * from './lib/components/breadcrumbs/breadcrumbs.component';
 export * from './lib/components/notifications/toast-service';
+export * from './lib/components/submit-button.component';
