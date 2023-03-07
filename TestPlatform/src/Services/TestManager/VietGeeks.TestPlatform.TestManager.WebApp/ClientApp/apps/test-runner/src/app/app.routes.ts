3d1ab4dd3509606a2946f7759a71f4a329e724c5
tau <tau.dangngoc@teragroup.io>
2023-03-08 00:34:03 +0700

    Init start page

--- TestPlatform/src/Services/TestManager/VietGeeks.TestPlatform.TestManager.WebApp/ClientApp/apps/test-runner/src/app/app.routes.ts
+++ TestPlatform/src/Services/TestManager/VietGeeks.TestPlatform.TestManager.WebApp/ClientApp/apps/test-runner/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: 'test',
+        loadChildren: () => import('./proctor/proctor-routing.module').then(m => m.ProctorRoutingModule)
+    },
+    {
+        path: '**',
+        redirectTo: 'test/start'
+    }
+];
