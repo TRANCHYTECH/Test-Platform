1cb3a6106268d0498cd2d2bf7a58de6602b92f64
tau <tau.dangngoc@teragroup.io>
2023-03-08 22:37:35 +0700

    Add configuration api to test runner app

---
+++ TestPlatform/src/Services/TestRunner/VietGeeks.TestPlatform.TestRunner.WebApp/Models/AppConfigurationSettings.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace VietGeeks.TestPlatform.TestRunner.WebApp.Models
+{
+    public class AppConfigurationSettings
+    {
+        public string TestRunnerApiBaseUrl { get; set; } = default!;
+    }
+}
+
