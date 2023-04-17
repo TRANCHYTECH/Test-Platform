c880af40b110e65d560b36eaf72c2d4d4d2f47cf
tau <tau.dangngoc@teragroup.io>
2023-04-17 07:32:32 +0700

    fix prod build mode for test manager

--- TestPlatform/src/Services/TestManager/VietGeeks.TestPlatform.TestManager.WebApp/ClientApp/apps/test-manager/src/environments/environment.prod.ts
+++ TestPlatform/src/Services/TestManager/VietGeeks.TestPlatform.TestManager.WebApp/ClientApp/apps/test-manager/src/environments/environment.prod.ts
@@ -1,13 +1,14 @@
 
 export const environment = {
     production: true,
-    testManagerApiBaseUrl: "https://dev.api.testmaster.io",
+    testManagerApiBaseUrl: "",
+    accountManagerApiBaseUrl: "",
     testRunnerBaseUrl: "",
     auth: {
-        domain: 'dev-kz3mgkb4xl50tobe.us.auth0.com',
-        clientId: 'SaMKFPQSdPoAyooI3zF6oiz6zdZ6hU18',
-        audience:  'http://test-manager',
-        scope: 'read:current_user',
-        intercepters: ['https://dev.api.testmaster.io/*']
+        domain: '',
+        clientId: '',
+        audience:  '',
+        scope: '',
+        intercepters: []
     }
 }
